58137cbcd35850575b2cf0cf20a4eeec52899fd9
vinayreddie <62783830+vinayreddie@users.noreply.github.com>
2021-08-06 23:32:11 -0400

    Update RatingAdjustmentService.cs

--- RatingAdjustmentService/RatingAdjustmentService.cs
+++ RatingAdjustmentService/RatingAdjustmentService.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace RatingAdjustment.Services
 {
@@ -22,7 +22,8 @@ namespace RatingAdjustment.Services
         void SetPercentPositive(double stars)
         {
             // TODO: Implement this!
-            _percent_positive = (stars / 100);
+            var t = 20 * stars;
+            _percent_positive = t / 100;
         }
 
         /**
@@ -30,9 +31,7 @@ namespace RatingAdjustment.Services
          */
         void SetQ(double number_of_ratings)
         {
-         // TODO: Implement this!
-            double t = Z * (Math.Sqrt(_percent_positive * (1 - _percent_positive) + Math.Sqrt(Z) / 4 * number_of_ratings) /
-number_of_ratings);
+            _q = Z * Math.Sqrt((_percent_positive * (1 - _percent_positive) + Z*Z / (4 * number_of_ratings)) / number_of_ratings);
         }
 
         /** Adjusted lower bound
@@ -41,11 +40,17 @@ number_of_ratings);
          * 
          * Returns: a double, up to 5
          */
-        public double Adjust(double stars, double number_of_ratings) {
+        public double Adjust(double stars, double number_of_ratings)
+        {
             // TODO: Implement this!
-            double lowerbound = (_percent_positive+( Z*Z/2*number_of_ratings)-stars)/(1+Z*Z/number_of_ratings);
-return lowerbound;
-            return 0.0;
+            SetPercentPositive(stars);
+            SetQ(number_of_ratings);
+            double lowerbound = (_percent_positive + (Z*Z / (2 * number_of_ratings)) - _q) / (1 + (Z*Z / number_of_ratings)) * MAX_STARS;
+            if (lowerbound <= stars)
+                return lowerbound;
+            else
+                return stars;
+        
         }
     }
 }
